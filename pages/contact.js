8aea05809c337fcb0d6360fa668e6aa147d9e711
Marigameo <mariappangameo@gmail.com>
2021-08-06 13:18:16 +0530

    initial project setup & creating pages in next

---
+++ pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function about() {
+    return (
+        <>
+        <Head>
+        <title>Contact page</title>
+        </Head>
+        <div style={{margin: '3rem', fontSize: '24px'}}>
+            Contact page
+        </div>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          I'm learning next.js
         </p>
 
         <div className={styles.grid}>
